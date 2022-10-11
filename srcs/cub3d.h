24ab1377f399e1f1e0cac8f7fa37e2a17dd8e221
littley-y <hyunyoungtae@gmail.com>
2022-10-11 19:12:17 +0900

    1011 linux

--- srcs/cub3d.h
+++ srcs/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: littley <littley@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 12:13:17 by yehyun            #+#    #+#             */
-/*   Updated: 2022/10/07 19:38:34 by littley          ###   ########.fr       */
+/*   Updated: 2022/10/07 21:18:22 by littley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,16 @@
 
 # define PRESS 2
 # define RELEASE 3
-# define KEY_W 13
-# define KEY_A 0
-# define KEY_S 1
-# define KEY_D 2
-# define KEY_M 46
-# define KEY_LEFT 123
-# define KEY_RIGHT 124
-# define KEY_ESC 53
-# define KEY_SPACE 49
-# define RED_BUTTON 17
+# define KEY_W 119
+# define KEY_A 97
+# define KEY_S 115
+# define KEY_D 100
+# define KEY_M 109
+# define KEY_LEFT 65361
+# define KEY_RIGHT 65363
+# define KEY_ESC 65307
+# define RED_BUTTON 79
+# define KEY_SPACE 32
 
 # define P_WIDTH 64
 # define P_HEIGHT 64
